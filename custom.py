02126df2b7a546840c8beb52dc44db4339bf60a0
Zhilei Ma <80358105+zmaalgorithmia@users.noreply.github.com>
2022-12-19 12:12:51 -0500

    remove read_input_data hook.

--- custom.py
+++ custom.py
@@ -4,11 +4,6 @@ import io
 from pathlib import Path
 
 
-def read_input_data(input_binary_data):
-    df = pd.read_csv(io.BytesIO(input_binary_data))
-    return df
-
-
 def load_model(code_dir):
     code_path = Path(code_dir) / Path("random_forest_model.pkl")
 
